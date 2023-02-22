e9dbb9163dc33ae2f2d6692cb912ea9f932e2f10
gcurtis1092@gmail.com <gcurtis1092@gmail.com>
2023-02-21 19:52:58 -0700

    Week 7 Edits

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,15 @@
-import html from "html-literal"
+import html from "html-literal";
 
-export default () => html`
-<section id="jumbotron">
+export default state => html`
+  <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
     <a href="index.html">"Call to Action" "Button"</a>
-  </section>`;
+  </section>
+
+  <h3>Today's weather</h3>
+  <p>
+    The current weather in ${state.weather.city} is
+    ${state.weather.description}. The temp is ${state.weather.temp} degrees and
+    feels like ${state.weather.feelsLike}
+  </p>
+`;
